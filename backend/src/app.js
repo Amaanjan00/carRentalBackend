95a2a184670e250bf191e7f7d8f122b7b9124289
Amaanjan00 <amaan.jan@bba.christuniversity.in>
2025-09-10 01:39:29 +0400

    frontend initialised

--- backend/src/app.js
+++ backend/src/app.js
@@ -12,7 +12,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
-app.use(cors());
+app.use(cors(
+    { 
+        origin: 'http://localhost:3000', 
+    }
+));
 app.use(cookieParser());
 
 // Define your routes here
